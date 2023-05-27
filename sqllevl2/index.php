aba521bfd82ded43b2c6fa513d4aa4eb5e910815
Thehackingverse <86924237+thehackingverse@users.noreply.github.com>
2023-05-27 21:21:35 +0530

    Add files via upload

---
+++ sqllevl2/index.php
@@ -0,0 +1,110 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+if(!$conn) {
+	die("Unable to connect");
+}
+if($_POST) {
+	$uname = $_POST["username"];
+	$pass = $_POST["password"];
+    $uname = mysqli_real_escape_string($conn, $uname);//test or 1=1
+	$sql = "SELECT * FROM users WHERE username = '$uname'  AND password = '$pass'";
+	$result = mysqli_query($conn, $sql);
+	if(mysqli_num_rows($result) == 1) {
+		header("Location: welcome.php");
+	} else {
+		echo "Incorrect Username/Password";
+	}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sqlinjection lab</title>
+	<style type="text/css">
+		input[type=text],input[type=password] {
+			padding: 16px;
+			margin: 8px;
+			border: 1px solid #f1f1f1;
+			letter-spacing: 1px;
+			border-radius: 3px;
+			width: 240px;
+		}
+		input[type=submit] {
+			margin-left: 8px;
+			width: 274px;
+			border-radius: 3px;
+			border: 1px solid #4285f4;
+			background-color: #4285f4;
+			padding: 16px;
+			color: white;
+			font-weight: 600;
+			cursor: pointer;
+		}
+		input[type=button] {
+			margin-left: 8px;
+			width: 274px;
+			border-radius: 3px;
+			border: 1px solid #4285f4;
+			background-color: #FFD700;
+			padding: 16px;
+			color: white;
+			font-weight: 600;
+			cursor: pointer;
+		}
+		body{
+             
+			height: 100%;
+            
+			background-image:url('spaceinjection.gif');
+            background-repeat:no-repeat;
+            background-size: cover;
+			background-position: center;
+		}
+		h1{
+
+			color:white;
+			font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+		}
+
+
+		.hide {
+  display: none;
+}
+    
+.myDIV:hover + .hide {
+  display: block;
+  color: white;
+}
+	</style>
+</head>
+<body>
+    <br><br>
+    <br>
+	<center><h1>Welcome to  sql injection lab.<h1></center>
+    <br>
+    <br>
+    <br>
+
+    <br>
+	<br>
+    <br><br>
+    <br>
+	<br>
+    <br>
+	<center><h1>Login Panel</h1></center>
+	<center><form action="index.php" method="POST" autocomplete="off">
+		<input type="text" name="username" placeholder="Username" /><br />
+		<input type="password" name="password" placeholder="password" /><br />
+		<input type="submit" name="login" value="LOGIN" />
+	</form></center>
+	<br>
+	<br>
+	<center><div class="myDIV"><input type="button" name="hint" value="Hint"></div>
+    <div class="hide">Use your power of hacking & gain access.</div></center>
+
+</body>
+</html>
---
+++ sqllevl2/welcome.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sqlinjection lab</title>
+</head>
+
+<style>
+
+body{
+             
+             height: 100%;
+             
+             background-image:url('spaceinjection.gif');
+             background-repeat:no-repeat;
+             background-size: cover;
+             
+         }
+
+
+h1{
+
+    color:white;
+	font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+
+}
+input[type=submit] {
+			margin-left: 8px;
+			width: 274px;
+			border-radius: 3px;
+			border: 1px solid #4285f4;
+			background-color: #4285f4;
+			padding: 16px;
+			color: white;
+			font-weight: 600;
+			cursor: pointer;
+		}
+
+</style>
+<body>
+    <br>
+    <br>
+    <center><h1>Welcome to  sql injection lab.<h1></center>
+    <br>
+    <br>
+    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <center><h1>Welcome Back Admin
+</h1></center> 
+    <center><h1>Well done hacker you have done the injection successfully.</h1></center> 
+    <center><a href="index.php"><input type="submit" name="logout" value="LOGOUT"  /></a>&nbsp; <a href="../sqllevl3/index.php"><input type="submit" name="Proceed for Level 2" value="Proceed for Level 3" /></a></center>
+</body>
+</html>
